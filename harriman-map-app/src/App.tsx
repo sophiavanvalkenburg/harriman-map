432b1b4b0333376e59cee3d405f8f41e6ceb2a85
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2024-08-12 22:27:15 -0400

    fix types for initial map

--- harriman-map-app/src/App.tsx
+++ harriman-map-app/src/App.tsx
@@ -1,4 +1,5 @@
-import { useRef, useEffect, useState } from 'react';
+//import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import './App.css'
 
@@ -14,18 +15,16 @@ function Map() {
     mapboxgl.accessToken = 'pk.eyJ1IjoibWVvd3lwdXJyIiwiYSI6ImNsemxlNTE0ZzAxbWUybG9qdHk1aGNlbHkifQ.WEaFTpVEow-9nOl0__ZqeA';
     
     const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-74.076);
-    const [lat, setLat] = useState(41.2309);
-    const [zoom, setZoom] = useState(11.15);
+    const map = useRef<mapboxgl.Map|null>(null);
 
     useEffect(() => {
+      if (!mapContainer.current) return;
       if (map.current) return; // initialize map only once
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/meowypurr/clzpy8ohh00bn01qg7rsa8u3u',
-        center: [lng, lat],
-        zoom: zoom
+        center: [-74.076, 41.2309],
+        zoom: 11.15
       });
     });
 
