246ce3832544f1dc4a5a35d497db6f0c05f36fab
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2024-08-12 22:03:33 -0400

    add mapbox

--- harriman-map-app/src/App.tsx
+++ harriman-map-app/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="App">
+     <Map />
+    </div>
+  );
+}
+
+function Map() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWVvd3lwdXJyIiwiYSI6ImNsemxlNTE0ZzAxbWUybG9qdHk1aGNlbHkifQ.WEaFTpVEow-9nOl0__ZqeA';
+    
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-74.076);
+    const [lat, setLat] = useState(41.2309);
+    const [zoom, setZoom] = useState(11.15);
+
+    useEffect(() => {
+      if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/meowypurr/clzpy8ohh00bn01qg7rsa8u3u',
+        center: [lng, lat],
+        zoom: zoom
+      });
+    });
+
+    
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div ref={mapContainer} className="map-container" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
 }
 
 export default App
