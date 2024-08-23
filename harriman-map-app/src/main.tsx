d4d4a03d481c33acd7b909af87be0a7daaec3bf6
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2024-08-23 11:53:11 -0400

    use Map instead of App in main

--- harriman-map-app/src/main.tsx
+++ harriman-map-app/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'mapbox-gl/dist/mapbox-gl.css';
-import App from './App.tsx'
+import Map from './Map.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Map />
   </StrictMode>,
 )
